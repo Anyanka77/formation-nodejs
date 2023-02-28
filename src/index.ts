02e3f520197267afd9aa7228ffd060a7781d8aa4
Anyanka77 <ylasseaux@gmail.com>
2023-02-28 14:52:46 +0100

    config dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 console.log(
   "Dans une Galaxie lointaine, très lointaine... Un petit jawa curieux se promène..."
 );
+
+// On affiche la variable d'environement
+console.log(process.env.NODE_ENV);
+console.log(process.env.TZ);
+console.log(process.env.HOST);
+console.log(process.env.PORT);
